4f55f478e13ae16e715519b3e2a3fc8eed0a9dca
floweryclover <floweryclover@gmail.com>
2024-11-26 23:09:43 +0900

    [Feat] Timer UI 구현

--- FarmDefencer/Assets/Scripts/01_Farm/UI/FarmUI.cs
+++ FarmDefencer/Assets/Scripts/01_Farm/UI/FarmUI.cs
@@ -11,13 +11,16 @@ public class FarmUI : MonoBehaviour
 	private HarvestInventory _harvestInventory;
 	public HarvestInventory HarvestInventory => _harvestInventory;
 
+	private TimerUI _timerUI;
+
 	private void Awake()
 	{
 		_coinsUI = GetComponentInChildren<CoinsUI>();
 		_harvestAnimationPlayer = GetComponentInChildren<HarvestAnimationPlayer>();
 		_wateringCan = GetComponentInChildren<WateringCan>();
 		_harvestInventory = GetComponentInChildren<HarvestInventory>();
-
+		_timerUI = GetComponentInChildren<TimerUI>();
+		_timerUI.SetClockhand(0.5f);
 		_coinsUI.UpdateCoinText(ResourceManager.Instance.Gold);
 		ResourceManager.Instance.OnGoldChanged += _coinsUI.UpdateCoinText;
 	}
---
+++ FarmDefencer/Assets/Scripts/01_Farm/UI/TimerUI.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerUI : MonoBehaviour
+{
+	public float StartAngle = 170f;
+	public float EndAngle = 360f;
+	private Image _clockHand;
+
+	/// <summary>
+	/// ZeroDegree와 FullDegree 중 어느 위치를 가리켜야 하는지를 설정합니다.
+	/// 0~1 사이의 값이며, 0일경우 ZeroDegree, 1일경우 FullDegree에 위치하게 됩니다.
+	/// </summary>
+	/// <param name="ratio"></param>
+	public void SetClockhand(float ratio)
+	{
+		ratio = Mathf.Clamp01(ratio);
+
+		float angle = Mathf.Lerp(StartAngle, EndAngle, ratio);
+
+		_clockHand.rectTransform.localRotation = Quaternion.Euler(0f, 0f, -angle); // UI의 Z축 회전
+	}
+
+	private void Awake()
+	{
+		_clockHand = transform.Find("Clockhand").GetComponent<Image>();
+	}
+}
