9e38366b5522a03818df6615c4da2494bb8c0380
floweryclover <floweryclover@gmail.com>
2025-01-07 16:27:16 +0900

    [Refactor] 변수명 명확하게 수정, 성장 시간 농작물 표대로 설정
    - CropCarrot의 변수명 중 DeadToSeedSeconds를 DeadToLockSeconds로 명확하게 변경

--- FarmDefencer/Assets/Scripts/01_Farm/Crop/CropCarrot.cs
+++ FarmDefencer/Assets/Scripts/01_Farm/Crop/CropCarrot.cs
@@ -11,7 +11,7 @@ public class CropCarrot : Crop
 	public Sprite LockedSprite;
 	[Space]
 	public float PlantToDeadSeconds = 300.0f;
-	public float DeadToSeedSeconds = 300.0f;
+	public float DeadToLockSeconds = 300.0f;
 	private SpriteRenderer _spriteRenderer;
 
 	public override void OnTap()
@@ -49,7 +49,7 @@ public class CropCarrot : Crop
 		{
 			return;
 		}
-		if (WaterWaitingSeconds < PlantToDeadSeconds + DeadToSeedSeconds && WaterStored == 0.0f)
+		if (WaterWaitingSeconds < PlantToDeadSeconds + DeadToLockSeconds && WaterStored == 0.0f)
 		{
 			WaterStored += MatureAgeSeconds * 1.1f; // 딱 맞아떨어지게 하면 99%에서 물 다시 달라고 할 수 있음
 		}
@@ -81,7 +81,7 @@ public class CropCarrot : Crop
 			}
 			else
 			{
-				if (WaterWaitingSeconds >= PlantToDeadSeconds+DeadToSeedSeconds)
+				if (WaterWaitingSeconds >= PlantToDeadSeconds+DeadToLockSeconds)
 				{
 					State = CropState.Locked;
 				}
