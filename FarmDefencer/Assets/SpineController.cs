0e63ef546c5985a89569a78a4249b3a0ddb65359
joonyle99 <gongjunyeol@gmail.com>
2025-01-19 19:28:11 +0900

    [Feat] 스파인 컨트롤러 생성

---
+++ FarmDefencer/Assets/SpineController.cs
@@ -0,0 +1,30 @@
+using Spine;
+using Spine.Unity;
+using UnityEngine;
+
+public class SpineController : MonoBehaviour
+{
+    [Header("──────── SpineController ────────")]
+    [Space]
+
+    private SkeletonAnimation _skeletonAnimation;
+    private Spine.AnimationState _spineAnimationState;
+    private Spine.Skeleton _skeleton;
+
+    private void Awake()
+    {
+        _skeletonAnimation = GetComponent<SkeletonAnimation>();
+
+        _spineAnimationState = _skeletonAnimation.AnimationState;
+        _skeleton = _skeletonAnimation.Skeleton;
+    }
+
+    public void SetAnimation(string animationName, bool loop)
+    {
+        _spineAnimationState.SetAnimation(0, animationName, loop);
+    }
+    public void AddAnimation(string animationName, bool loop, float delay = 0f)
+    {
+        _spineAnimationState.AddAnimation(0, animationName, loop, delay);
+    }
+}
