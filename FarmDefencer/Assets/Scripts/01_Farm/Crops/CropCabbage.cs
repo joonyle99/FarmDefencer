525111d302e304574885d2ccd7004f5844bd6371
floweryclover <floweryclover@gmail.com>
2025-02-25 22:35:24 +0900

    [Refactor] FP를 이용한 CropCabbage 리팩토링

--- FarmDefencer/Assets/Scripts/01_Farm/Crops/CropCabbage.cs
+++ FarmDefencer/Assets/Scripts/01_Farm/Crops/CropCabbage.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
+using System.Collections.Generic;
+using System;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 /// <summary>
 /// 0번 자식에 수확 단계 흙을 표시하기 위한 SpriteRenderer 오브젝트를 할당하고,
@@ -7,226 +9,263 @@ using UnityEngine.UIElements;
 /// </summary>
 public class CropCabbage : Crop
 {
+	private struct CabbageState : ICommonCropState
+	{
+		public bool Planted { get; set; }
+		public float WaterWaitingSeconds { get; set; }
+		public float GrowthSeconds { get; set; }
+		public bool Watered { get; set; }
+		public bool Harvested { get; set; }
+		public float HoldingTime { get; set; }
+		public int RemainingQuota { get; set; }
+		public float Shake { get; set; }
+		public int ShakeCount { get; set; }
+		public bool WasLastShakeLeftSide { get; set; }
+	}
+
+	private enum CabbageStage
+	{
+		Seed,
+		Mature,
+		Harvested,
+
+		Stage1_BeforeWater,
+		Stage1_Dead,
+		Stage1_Growing,
+
+		Stage2_BeforeWater,
+		Stage2_Dead,
+		Stage2_Growing,
+	}
+
 	private const float DeltaShakeCriterion = 0.25f; // 배추 수확단계 흔들기 기준 (가로 방향 위치 델타)
 	private const int ShakeCountCriterion = 4; // 배추 수확 흔들기 횟수 기준
-	private const float Stage1GrowthSeconds = 10.0f;
-	private const float Stage2GrowthSeconds = 10.0f;
+	private const float Stage1_GrowthSeconds = 10.0f;
+	private const float Stage2_GrowthSeconds = 10.0f;
 
-	public Sprite SeedSprite;
-	[Space]
-	public Sprite Stage1BeforeWateringSprite;
-	public Sprite Stage1DeadSprite;
-	public Sprite Stage1AfterWateringSprite;
-	[Space]
-	public Sprite Stage2BeforeWateringSprite;
-	public Sprite Stage2DeadSprite;
-	public Sprite Stage2AfterWateringSprite;
-	[Space]
-	public Sprite HarvestedSprite;
-	[Space]
+	[SerializeField] private Sprite _seedSprite;
+	[SerializeField] private Sprite _harvestedSprite;
+
+	[SerializeField] private Sprite _stage1_beforeWaterSprite;
+	[SerializeField] private Sprite _stage1_deadSprite;
+	[SerializeField] private Sprite _stage1_growingSprite;
+
+	[SerializeField] private Sprite _stage2_beforeWaterSprite;
+	[SerializeField] private Sprite _stage2_deadSprite;
+	[SerializeField] private Sprite _stage2_growingSprite;
+
+
 	private SpriteRenderer _spriteRenderer; // 수확 단계를 제외한 모든 단계의 스프라이트 표시
+	private CabbageState _currentState;
 	private GameObject _harvestSoilLayerObject; // 0번 자식
 	private GameObject _harvestCropLayerObject; // 1번 자식
 
-	private int _shakeCount;
-	private bool _wasLastShakeLeft;
-	private bool _isSeed;
-	private bool _harvested;
-
 	public override void OnSingleTap(Vector2 worldPosition)
 	{
-		if (_isSeed)
-		{
-			_isSeed = false;
-			EffectPlayer.PlayTabEffect(worldPosition);
-			SoundManager.PlaySfx("SFX_plant_seed");
-		}
-		else if (_harvested)
-		{
-			if (HarvestHandler(1) > 0)
-			{
-				EffectPlayer.PlayTabEffect(worldPosition); 
-				_isSeed = true;
-			}
-		}
+		_currentState = HandleActionFillQuotaAndPlayEffectAt(
+
+			Effects,
+			GetQuota,
+			NotifyQuotaFilled,
+			OnSingleTapFunctions[GetCurrentStage(_currentState)], _currentState)
+
+			(transform.position, transform.position);
 	}
 
 	public override void OnWatering()
 	{
-		if (!_isSeed && !watered)
-		{
-			watered = true;
-			waterWaitingSeconds = 0.0f;
-			SoundManager.PlaySfx("SFX_water_oneshot");
-		}
+		_currentState = HandleActionFillQuotaAndPlayEffectAt(
+
+			Effects,
+			GetQuota,
+			NotifyQuotaFilled,
+			Water,
+			_currentState)
+
+			(transform.position, transform.position);
 	}
 
 	public override void OnSingleHolding(Vector2 initialPosition, Vector2 deltaPosition, bool isEnd, float deltaHoldTime)
 	{
-		if (isEnd)
-		{
-			ShakeMotion(0.0f);
-			_shakeCount = 0;
-			return;
-		}
+		_currentState = HandleActionFillQuotaAndPlayEffectAt(
 
-		if (growthSeconds >= Stage1GrowthSeconds + Stage2GrowthSeconds
-			&& !_harvested)
-		{
-			var deltaShake = deltaPosition.x;
-			ShakeMotion(deltaShake);
-			EffectPlayer.PlayHoldEffect(initialPosition + deltaPosition);
-			if (Mathf.Abs(deltaShake) > DeltaShakeCriterion)
+			Effects,
+			GetQuota,
+			NotifyQuotaFilled,
+			(beforeState)
+			=>
 			{
-				if (_shakeCount == 0)
-				{
-					EffectPlayer.PlayVfx("SoilParticle", transform.position);
-					_shakeCount += 1;
-					_wasLastShakeLeft = deltaShake < 0.0f;
-				}
-				else
-				{
-					if (deltaShake < 0.0f && !_wasLastShakeLeft
-						|| deltaShake > 0.0f && _wasLastShakeLeft)
-					{
-						EffectPlayer.PlayVfx("SoilParticle", transform.position);
-						_shakeCount += 1;
-						_wasLastShakeLeft = !_wasLastShakeLeft;
-					}
-				}
+				return OnSingleHoldingFunctions[GetCurrentStage(_currentState)](_currentState, initialPosition, deltaPosition, isEnd, deltaHoldTime);
+			},
+			_currentState)
 
-				if (_shakeCount >= ShakeCountCriterion)
-				{
-					_harvested = true;
-					SoundManager.PlaySfx("SFX_harvest");
-				}
-			}
-		}
+			(transform.position, transform.position);
 	}
 
 	public override void OnFarmUpdate(float deltaTime)
 	{
-		_harvestSoilLayerObject.SetActive(false);
-		_harvestCropLayerObject.SetActive(false);
+		var currentStage = GetCurrentStage(_currentState);
+		ApplySpriteTo(currentStage)(_spriteRenderer);
 
-		if (_isSeed)
-		{
-			watered = false;
-			waterWaitingSeconds = 0.0f;
-			growthSeconds = 0.0f;
-			_harvested = false;
-			_wasLastShakeLeft = false;
-			_shakeCount = 0;
-
-			if (_spriteRenderer.sprite != SeedSprite)
-			{
-				_spriteRenderer.sprite = SeedSprite;
-			}
-
-			return;
-		}
-
-		if (_harvested)
-		{
-			if (_spriteRenderer.sprite != HarvestedSprite)
-			{
-				_spriteRenderer.sprite = HarvestedSprite;
-			}
-
-			return;
-		}
-
-
-		if (growthSeconds >= Stage1GrowthSeconds + Stage2GrowthSeconds) // 모두 성장한 단계
+		if (currentStage == CabbageStage.Mature)
 		{
 			_harvestSoilLayerObject.SetActive(true);
 			_harvestCropLayerObject.SetActive(true);
-			_spriteRenderer.sprite = null;
+			_harvestCropLayerObject.transform.localEulerAngles = _harvestCropLayerObject.transform.localEulerAngles = new Vector3(0.0f, 0.0f, Mathf.Atan(-_currentState.Shake) * 90.0f / Mathf.PI);
 		}
-		else if (growthSeconds >= Stage1GrowthSeconds) // 성장 2단계
+		else
 		{
-			if (!watered) // 물 안준 상태
-			{
-				waterWaitingSeconds += deltaTime;
-				if (waterWaitingSeconds >= WaterWaitingDeadSeconds + WaterWaitingResetSeconds)
-				{
-					_isSeed = true;
-				}
-				else if (waterWaitingSeconds >= WaterWaitingDeadSeconds)
-				{
-					if (_spriteRenderer.sprite != Stage2DeadSprite)
-					{
-						_spriteRenderer.sprite = Stage2DeadSprite;
-					}
-				}
-				else
-				{
-					if (_spriteRenderer.sprite != Stage2BeforeWateringSprite)
-					{
-						_spriteRenderer.sprite = Stage2BeforeWateringSprite;
-					}
-				}
-			}
-			else // 물 준 상태
-			{
-				growthSeconds += deltaTime;
-				if (_spriteRenderer.sprite != Stage2AfterWateringSprite)
-				{
-					_spriteRenderer.sprite = Stage2AfterWateringSprite;
-				}
-			}
+			_harvestSoilLayerObject.SetActive(false);
+			_harvestCropLayerObject.SetActive(false);
 		}
-		else // 성장 1단계
-		{
-			if (!watered) // 물 안준 상태
-			{
-				waterWaitingSeconds += deltaTime;
-				if (waterWaitingSeconds >= WaterWaitingDeadSeconds + WaterWaitingResetSeconds)
-				{
-					_isSeed = true;
-				}
-				else if (waterWaitingSeconds >= WaterWaitingDeadSeconds)
-				{
-					if (_spriteRenderer.sprite != Stage1DeadSprite)
-					{
-						_spriteRenderer.sprite = Stage1DeadSprite;
-					}
-				}
-				else
-				{
-					if (_spriteRenderer.sprite != Stage1BeforeWateringSprite)
-					{
-						_spriteRenderer.sprite = Stage1BeforeWateringSprite;
-					}
-				}
-			}
-			else // 물 준 상태
-			{
-				growthSeconds += deltaTime;
 
-				if (growthSeconds >= Stage1GrowthSeconds)
-				{
-					watered = false;
-				}
+		_currentState = HandleActionFillQuotaAndPlayEffectAt(
 
-				if (_spriteRenderer.sprite != Stage1AfterWateringSprite)
-				{
-					_spriteRenderer.sprite = Stage1AfterWateringSprite;
-				}
-			}
-		}
+			Effects,
+			GetQuota,
+			NotifyQuotaFilled,
+			(beforeState)
+			=>
+			{
+				return OnFarmUpdateFunctions[currentStage](_currentState, deltaTime);
+			},
+			_currentState)
+
+			(transform.position, transform.position);
 	}
 
 	private void Awake()
 	{
-		_isSeed = true;
 		_spriteRenderer = GetComponent<SpriteRenderer>();
 		_harvestSoilLayerObject = transform.GetChild(0).gameObject;
 		_harvestCropLayerObject = transform.GetChild(1).gameObject;
 	}
 
-	private void ShakeMotion(float deltaShake)
+	// 이하 함수 빌딩 블록
+
+	private static CabbageStage GetCurrentStage(CabbageState state) => state switch
 	{
-		_harvestCropLayerObject.transform.localEulerAngles = new Vector3(0.0f, 0.0f, Mathf.Atan(-deltaShake) * 90.0f / Mathf.PI);
-	}
+		{ Planted: false } => CabbageStage.Seed,
+		{ Harvested: true } => CabbageStage.Harvested,
+		{ GrowthSeconds: >= Stage1_GrowthSeconds + Stage2_GrowthSeconds } => CabbageStage.Mature,
+
+		{ GrowthSeconds: >= Stage1_GrowthSeconds, WaterWaitingSeconds: >= WaterWaitingDeadSeconds + WaterWaitingResetSeconds } => CabbageStage.Seed,
+		{ GrowthSeconds: >= Stage1_GrowthSeconds, WaterWaitingSeconds: >= WaterWaitingDeadSeconds } => CabbageStage.Stage2_Dead,
+		{ GrowthSeconds: >= Stage1_GrowthSeconds, Watered: true } => CabbageStage.Stage2_Growing,
+		{ GrowthSeconds: >= Stage1_GrowthSeconds } => CabbageStage.Stage2_BeforeWater,
+
+		{ WaterWaitingSeconds: >= WaterWaitingDeadSeconds + WaterWaitingResetSeconds } => CabbageStage.Seed,
+		{ WaterWaitingSeconds: >= WaterWaitingDeadSeconds } => CabbageStage.Stage1_Dead,
+		{ Watered: true } => CabbageStage.Stage1_Growing,
+		{ } => CabbageStage.Stage1_BeforeWater,
+	};
+
+	private static readonly Dictionary<CabbageStage, Func<CabbageState, float, CabbageState>> OnFarmUpdateFunctions = new Dictionary<CabbageStage, Func<CabbageState, float, CabbageState>>
+	{
+		{CabbageStage.Seed, (currentState, deltaTime) => Reset(currentState) },
+		{CabbageStage.Mature, (currentState, deltaTime) => DoNothing(currentState) },
+		{CabbageStage.Harvested, (currentState, deltaTime) => DoNothing(currentState) },
+
+		{CabbageStage.Stage2_Dead, WaitWater },
+		{CabbageStage.Stage2_BeforeWater, WaitWater },
+		{CabbageStage.Stage2_Growing, Grow },
+
+		{CabbageStage.Stage1_Dead, WaitWater },
+		{CabbageStage.Stage1_BeforeWater, WaitWater },
+		{
+			CabbageStage.Stage1_Growing,
+			(beforeState, deltaTime) =>
+			{
+				var nextState = Grow(beforeState, deltaTime);
+				if (nextState.GrowthSeconds >= Stage1_GrowthSeconds)
+				{
+					nextState.Watered = false;
+				}
+				return nextState;
+			}
+		},
+	};
+
+	private static readonly Dictionary<CabbageStage, Func<CabbageState, CabbageState>> OnSingleTapFunctions = new Dictionary<CabbageStage, Func<CabbageState, CabbageState>>
+	{
+		{CabbageStage.Seed, Plant },
+		{CabbageStage.Harvested, (beforeState) => FillQuotaUpto(beforeState, 1) },
+
+		{CabbageStage.Mature, DoNothing },
+		{CabbageStage.Stage2_Dead, DoNothing },
+		{CabbageStage.Stage2_BeforeWater, DoNothing },
+		{CabbageStage.Stage2_Growing, DoNothing },
+		{CabbageStage.Stage1_Dead, DoNothing },
+		{CabbageStage.Stage1_BeforeWater, DoNothing },
+		{CabbageStage.Stage1_Growing, DoNothing },
+	};
+
+	
+	private static readonly Dictionary<CabbageStage, Func<CabbageState, Vector2, Vector2, bool, float, CabbageState>> OnSingleHoldingFunctions = new Dictionary<CabbageStage, Func<CabbageState, Vector2, Vector2, bool, float, CabbageState>>
+	{
+		{CabbageStage.Seed, DoNothing_OnSingleHolding },
+		{CabbageStage.Harvested, DoNothing_OnSingleHolding },
+		{CabbageStage.Stage2_BeforeWater, DoNothing_OnSingleHolding },
+		{CabbageStage.Stage2_Dead, DoNothing_OnSingleHolding },
+		{CabbageStage.Stage2_Growing, DoNothing_OnSingleHolding },
+		{CabbageStage.Stage1_BeforeWater, DoNothing_OnSingleHolding },
+		{CabbageStage.Stage1_Dead, DoNothing_OnSingleHolding },
+		{CabbageStage.Stage1_Growing, DoNothing_OnSingleHolding },
+		{
+			CabbageStage.Mature,
+			(beforeState, initialWorldPosition, deltaPosition, isEnd, deltaHoldTime)
+			=>
+			{
+				var nextState = beforeState;
+				if (Math.Abs(deltaPosition.x) > DeltaShakeCriterion)
+				{
+					if (beforeState.ShakeCount == 0)
+					{
+						nextState.ShakeCount += 1;
+						nextState.WasLastShakeLeftSide = deltaPosition.x < 0.0f;
+					}
+					else
+					{
+						if (beforeState.WasLastShakeLeftSide && deltaPosition.x > 0.0f
+						|| !beforeState.WasLastShakeLeftSide && deltaPosition.x < 0.0f)
+						{
+							nextState.ShakeCount += 1;
+							nextState.WasLastShakeLeftSide = !beforeState.WasLastShakeLeftSide;
+						}
+					}
+
+					if (nextState.ShakeCount >= ShakeCountCriterion)
+					{
+						nextState.Harvested = true;
+					}
+				}
+				return nextState;
+			}
+		},
+	};
+
+	[Pure]
+	private Action<SpriteRenderer> ApplySpriteTo(CabbageStage stage) => stage switch
+	{
+		CabbageStage.Seed when _spriteRenderer.sprite != _seedSprite => (spriteRenderer) => spriteRenderer.sprite = _seedSprite,
+		CabbageStage.Mature when _spriteRenderer.sprite != null => (spriteRenderer) => spriteRenderer.sprite = null,
+		CabbageStage.Harvested when _spriteRenderer.sprite != _harvestedSprite => (spriteRenderer) => spriteRenderer.sprite = _harvestedSprite,
+
+		CabbageStage.Stage2_Dead when _spriteRenderer.sprite != _stage2_deadSprite => (spriteRenderer) => spriteRenderer.sprite = _stage2_deadSprite,
+		CabbageStage.Stage2_BeforeWater when _spriteRenderer.sprite != _stage2_beforeWaterSprite => (spriteRenderer) => spriteRenderer.sprite = _stage2_beforeWaterSprite,
+		CabbageStage.Stage2_Growing when _spriteRenderer.sprite != _stage2_growingSprite => (spriteRenderer) => spriteRenderer.sprite = _stage2_growingSprite,
+
+		CabbageStage.Stage1_Dead when _spriteRenderer.sprite != _stage1_deadSprite => (spriteRenderer) => spriteRenderer.sprite = _stage1_deadSprite,
+		CabbageStage.Stage1_BeforeWater when _spriteRenderer.sprite != _stage1_beforeWaterSprite => (spriteRenderer) => spriteRenderer.sprite = _stage1_beforeWaterSprite,
+		CabbageStage.Stage1_Growing when _spriteRenderer.sprite != _stage1_growingSprite => (spriteRenderer) => spriteRenderer.sprite = _stage1_growingSprite,
+
+		_ => (_) => { }
+	};
+
+	private static List<(Func<CabbageState, CabbageState, bool>, Action<Vector2, Vector2>)> Effects = new List<(Func<CabbageState, CabbageState, bool>, Action<Vector2, Vector2>)>
+	{
+		(WaterEffectCondition, WaterEffect),
+		(PlantEffectCondition, PlantEffect),
+		(HarvestEffectCondition, HarvestEffect)
+	};
 }
