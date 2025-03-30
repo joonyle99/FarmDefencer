6bf6f62f4d82f5be9471dac5a6e8275f59b25042
floweryclover <floweryclover@gmail.com>
2025-03-30 22:10:14 +0900

    [Feat] SurvivedMonsterPenalty와 맵별 페널티 오브젝트들 추가

---
+++ FarmDefencer/Assets/Scripts/01_Farm/SurvivedMonsterPenalty.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public sealed class CropProbabilityData
+{
+    [SerializeField] private ProductEntry targetCrop;
+    [SerializeField] private float probability;
+
+    public ProductEntry TargetCrop => targetCrop;
+    public float Probability => probability;
+}
+
+[CreateAssetMenu(fileName = "SurvivedMonsterPenalty", menuName = "Scriptable Objects/Farm/Survived Monster Penalty")]
+public sealed class SurvivedMonsterPenalty : ScriptableObject
+{
+    [System.Serializable]
+    public class MonsterPenaltyData
+    {
+        [SerializeField] private string monster;
+        [SerializeField] private CropProbabilityData[] cropProbabilities;
+
+        public string Monster => monster;
+        public CropProbabilityData[] CropProbabilityDatas => cropProbabilities;
+    }
+
+    [SerializeField] private MonsterPenaltyData[] monsterPenaltyDatas;
+
+    public MonsterPenaltyData[] MonsterPenaltyDatas => monsterPenaltyDatas;
+}
