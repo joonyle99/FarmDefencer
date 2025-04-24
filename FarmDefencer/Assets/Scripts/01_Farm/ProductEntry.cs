a5f854e3aaec9f21157e24e24dc17b86d7bf8ce2
floweryclover <floweryclover@gmail.com>
2025-04-24 16:27:29 +0900

    [Fix] ProductEntry의 productName이 productProductName으로 되어있던 것 수정

--- FarmDefencer/Assets/Scripts/01_Farm/ProductEntry.cs
+++ FarmDefencer/Assets/Scripts/01_Farm/ProductEntry.cs
@@ -4,10 +4,11 @@ using UnityEngine.Serialization;
 [CreateAssetMenu(fileName = "ProductEntry", menuName = "Scriptable Objects/Farm/CropEntry")]
 public sealed class ProductEntry : ScriptableObject
 {
-    [FormerlySerializedAs("productName")] [SerializeField] private string productProductName;
-    public string ProductName => productProductName;
+    [FormerlySerializedAs("productProductName")] [SerializeField] private string productName;
+    public string ProductName => productName;
     [SerializeField] private Sprite productSprite;
     public Sprite ProductSprite => productSprite;
+    
     [SerializeField] private int price;
     public int Price => price;
 }
