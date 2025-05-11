210780c2f0425fb7f22b486af3e2958898f87931
joonyle99 <gongjunyeol@gmail.com>
2025-05-11 18:30:17 +0900

    [Chore] 오디오 루프 테스트

---
+++ FarmDefencer/Assets/Scripts/AudioLoopTest.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class AudioLoopTest : MonoBehaviour
+{
+    public AudioSource audioSource1;
+    public AudioSource audioSource2;
+
+    private float _forest01 = 87.000f;
+    private float _forest02 = 106.666f;
+
+    private float _beach01 = 76.000f;
+    private float _beach02 = 96.000f;
+
+    private bool dirty = false;
+
+    private void Start()
+    {
+        audioSource1.Play();
+        audioSource1.time = _beach01;
+    }
+    private void Update()
+    {
+        if (dirty)
+        {
+            return;
+        }
+
+        if (audioSource1.time >= _beach02)
+        {
+            Debug.Log("지금 새로운 오디오 재생");
+
+            dirty = true;
+            audioSource2.Play();
+        }
+    }
+}
