c0d9503cb8e633545ab13b9cc128209dd66a7b41
joonyle99 <gongjunyeol@gmail.com>
2025-06-02 16:44:58 +0900

    [Chore] 레벨 데이터 헤더 추가

--- FarmDefencer/Assets/Scripts/02_TowerDefence/01.Tower/TowerLevelData.cs
+++ FarmDefencer/Assets/Scripts/02_TowerDefence/01.Tower/TowerLevelData.cs
@@ -3,16 +3,25 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "TowerLevelData", menuName = "Scriptable Objects/TowerDefence/TowerLevelData")]
 public class TowerLevelData : ScriptableObject
 {
+    [Header("1. 외형")]
     public Sprite Icon;
+
+    [Header("1. 외형")]
     public string Name;
     public string Description;
     public int Level;
+
+    [Header("3. 능력치")]
     public float AttackRate;
     public float SlowRate;
     public float SlowDuration;
     public int Damage;
+
+    [Header("4. 비용")]
     public int ValueCost;
     public int SellCost;
+
+    [Header("5. 사운드")]
     public AudioClip FireReady;
     public AudioClip FireShot;
 }
